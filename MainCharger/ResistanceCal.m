cc50164956e6d5979d61911f9513c1d2d25d565c
Bin Xiao <bin.phil.xiao@gmail.com>
2018-12-14 12:05:51 -0500

    modified:   ResistanceCal.m

--- MainCharger/ResistanceCal.m
+++ MainCharger/ResistanceCal.m
@@ -1,10 +1,11 @@
-TH_uu=8.2 #Over charge upper threshold 
-TH_ud=7.9 #Over charge lower threshold 
-TH_du=6.5 #Under charge upper threshold 
-TH_dd=6 #Under charge lower threshold 
-REF=2 #Reference of comparator
+TH_uu=8.2   #Over charge upper threshold 
+TH_ud=7.9   #Over charge lower threshold 
+TH_du=6.5   #Under charge upper threshold 
+TH_dd=6     #Under charge lower threshold 
+REF=2       #Reference of comparator
+R_u=20      #Upper resistors R1+R2   
 A=[REF, REF-TH_uu, REF-TH_uu, 0; REF, REF-TH_ud, REF-TH_ud, REF-TH_ud; REF-TH_du, REF-TH_du, 0, 0; REF-TH_dd, REF-TH_dd, REF-TH_dd, 0]
-C=[-40;-40;-40;-40]
-B=inv(A)*C
-B
+C=[-2*R_u, -2*R_u, -2*R_u, -2*R_u]
+R=inv(A)*transpose(C)
+R
 
