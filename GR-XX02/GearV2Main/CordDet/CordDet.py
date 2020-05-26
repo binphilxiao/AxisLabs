62b1e64a95be54e45e53863742a62558258486d5
Bin Xiao <bin.phil.xiao@gmail.com>
2020-05-26 13:48:58 -0400

    modified:   GR-XX02/GearV2Main/CordDet/CordDet.py

--- GR-XX02/GearV2Main/CordDet/CordDet.py
+++ GR-XX02/GearV2Main/CordDet/CordDet.py
@@ -5,12 +5,13 @@ import pandas as pd
 import math
 #---------------------Design Parameter---------------------
 Length_data = 8192
-Sample_rate = 200
-FFT_N = 256
+Sample_rate = 1000
+FFT_N = 1024
 Reference_freq = 20
 Ref_sig = 1 #1: data, 0: reference sin
-File_number = 3
+File_number = 17
 Log_FFT = 0 #1: Log y axis, 0: Raw value
+Filter_No =2 
 #---------------------Reading data----------------------------
 if (File_number == 1):
     df = pd.read_csv('thinCord-A-01.csv')
@@ -20,6 +21,20 @@ elif (File_number == 3):
     df = pd.read_csv('thinCord-A-03.csv')
 elif (File_number == 4):
     df = pd.read_csv('thickCord-A-04.csv')
+elif (File_number == 5):
+    df = pd.read_csv('thickCord-C-05.csv')
+elif (File_number == 6):
+    df = pd.read_csv('thickCord-C-06.csv')
+elif (File_number == 13):
+    df = pd.read_csv('thinCord-C-13.csv')
+elif (File_number == 14):
+    df = pd.read_csv('thinCord-C-14.csv')
+elif (File_number == 15):
+    df = pd.read_csv('thickCord-C-15.csv')
+elif (File_number == 16):
+    df = pd.read_csv('thinCord-C-16_3MFoilTape.csv')
+elif (File_number == 17):
+    df = pd.read_csv('thickCord-C-17_3MFoilTape.csv')
 else:
     df = pd.read_csv('thinCord-A-01.csv')
 y_raw= np.array(df['Channel1(V)'])
@@ -68,28 +83,58 @@ else:
 plt.legend(loc=1)
 plt.show()
 #---------------------Filter--------------------------
-#   Parameter created by Iowa Hills IIR
-#   http://www.iowahills.com/4IIRFilterPage.html
-#   Sample rate =   1k Hz
-#   Fc =            30.5 Hz
-#   Bw =            50.5 Hz
-#   N_Poles =       2
-#   Type =          Butterworth
-#   dB_gain =       20
-#   a0   1.000000000000000000
-#   a1   -1.894000291715878030
-#   a2   0.905724944304319823
-#   b0   -0.438395206537278315
-#   b1   0.000000000000000000
-#   b2   0.438395206537278315
-#   Y[n] = ∑(i=0,N)b[i]*X[n-i] - ∑(i=1,N)a[i]*Y[n-i] 
-N_Poles = 2
-b = (-0.438395206537278315,
-     0.000000000000000000,
-     0.438395206537278315)
-a = (1.000000000000000000,
-     -1.894000291715878030,
-     0.905724944304319823)
+'''
+Y[n] = ∑(i=0,N)b[i]*X[n-i] - ∑(i=1,N)a[i]*Y[n-i]
+'''
+
+if (Filter_No ==1 ):
+    '''
+    Filter #1
+    Parameter created by Iowa Hills IIR
+    http://www.iowahills.com/4IIRFilterPage.html
+    Sample rate =   1k Hz
+    Fc =            30.5 Hz
+    Bw =            50.5 Hz
+    N_Poles =       2
+    Type =          Butterworth
+    dB_gain =       20
+    '''
+    N_Poles = 2
+    b = (-0.438395206537278315,
+         0.000000000000000000,
+         0.438395206537278315)
+    a = (1.000000000000000000,
+         -1.894000291715878030,
+         0.905724944304319823)
+elif (Filter_No ==2):
+    N_Poles = 12
+    b = (1.489615116744448460E-3,
+         -1.279955762435121440E-2,
+         5.201511911554906220E-2,
+         -1.337711378643884560E-1,
+         2.459290802564359170E-1,
+         -3.456548777668058130E-1,
+         3.855835180242848990E-1,
+         -3.456548777668058570E-1,
+         2.459290802564359170E-1,
+         -1.337711378643884560E-1,
+         5.201511911554906220E-2,
+         -1.279955762435121880E-2,
+         1.489615116744449130E-3)
+    a = (1.000000000000000000E0,
+         -1.030773587993327300E1,
+         4.919456360761213180E1,
+         -1.437258737985348530E2,
+         2.862589625803847680E2,
+         -4.094418017618562720E2,
+         4.312277335020591630E2,
+         -3.369612066840528720E2,
+         1.938855438884546920E2,
+         -8.012059290597099090E1,
+         2.257313705027450100E1,
+         -3.893795236407037840E0,
+         3.110661286227708990E-1)
+
 regX = []
 regY = []
 y=[]
@@ -111,7 +156,7 @@ for j in range(FFT_N):
         regY[0] -= a[k]* regY[k]
     y[j] = regY[0]      
 x1 = np.linspace(-0.512, 0.512, FFT_N)
-plt.plot(x1[50:FFT_N], y[50:FFT_N], label = 'Filtered signal')
+plt.plot(x1[0:FFT_N], y[0:FFT_N], label = 'Filtered signal')
 plt.grid()
 plt.xlabel('Time (Sec)')
 plt.ylabel('Amplitude (V)')
